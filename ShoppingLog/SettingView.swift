495edbefe1afee8f3215079f5112b24446b80d41
Ryuga <ribereo.minami@gmail.com>
2023-10-28 13:53:53 +0900

    fix: #7 NavigationStackで囲い、.navigationTitleでタイトルをつけた。

--- ShoppingLog/SettingView.swift
+++ ShoppingLog/SettingView.swift
@@ -9,37 +9,41 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        List {
-            Section("App Settings") {
-                Text("テーマカラー設定")
-                Text("言語設定")
-            }
-            Section("Support") {
-                Text("運営からのお知らせ")
-                Text("ヘルプ")
-                Text("お問い合わせ")
-            }
-            Section("About ShoppingLog App") {
-                Text("利用規約")
-                Text("プライバシーポリシー")
-                Text("特定商取引法に基づく表示")
-            }
+        NavigationStack {
+            List {
+                Section("App Settings") {
+                    Text("テーマカラー設定")
+                    Text("言語設定")
+                }
+                Section("Support") {
+                    Text("運営からのお知らせ")
+                    Text("ヘルプ")
+                    Text("お問い合わせ")
+                }
+                Section("About ShoppingLog App") {
+                    Text("利用規約")
+                    Text("プライバシーポリシー")
+                    Text("特定商取引法に基づく表示")
+                }
 
 
-            Section {
-                //
-            } footer: {
-                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
-                    Image(systemName: "cart")
-                        .font(.headline)
-                    Text("©ShoppingLog")
-                        .font(.caption)
-                    Text("Ver. 1.00")
-                        .font(.caption2)
-                })
-                .frame(maxWidth: .infinity)
+                Section {
+                    //
+                } footer: {
+                    VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                        Image(systemName: "cart")
+                            .font(.headline)
+                        Text("©ShoppingLog")
+                            .font(.caption)
+                        Text("Ver. 1.00")
+                            .font(.caption2)
+                    })
+                    .frame(maxWidth: .infinity)
+                }
             }
-
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
+            .font(.headline)
         }
     }
 }
